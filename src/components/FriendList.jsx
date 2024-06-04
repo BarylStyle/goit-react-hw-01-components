8681277fb97c8fddf8e1be77adfff8899f16d5f7
BarylStyle <mateusz.consulting@gmail.com>
2024-06-04 13:22:43 +0200

    Update FriendList.jsx

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../Styles/FriendListItem.css'; 
+import '../Styles/FriendList.css'; 
 
 const FriendList = ({ friends }) => {
   return (
